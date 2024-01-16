1e29137027eff43fec9c8213cfc974fe6ed8416a
BrevenBennett <Breven.stars@gmail.com>
2024-01-16 12:56:44 -0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto">
+        <Sidebar />
+        {/* <PostsFeed /> */}
+        {/* <Trending /> */}
+      </div>
+    </>
+  );
+}
